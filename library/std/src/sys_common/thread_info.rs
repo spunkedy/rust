32a7ee0338b4ee6785021534ed72291ba3a25d20
Thom Chiovoloni <thom@shift.click>
2022-08-08 08:17:45 -0700

    Move ThreadId to its own thread local, to avoid need for a TLS destructor

--- library/std/src/sys_common/thread_info.rs
+++ library/std/src/sys_common/thread_info.rs
@@ -1,7 +1,7 @@
 #![allow(dead_code)] // stack_guard isn't used right now on all platforms
 #![allow(unused_unsafe)] // thread_local with `const {}` triggers this liny
 
-use crate::cell::RefCell;
+use crate::cell::{Cell, RefCell};
 use crate::sys::thread::guard::Guard;
 use crate::thread::{Thread, ThreadId};
 
@@ -10,7 +10,10 @@ struct ThreadInfo {
     thread: Thread,
 }
 
-thread_local! { static THREAD_INFO: RefCell<Option<ThreadInfo>> = const { RefCell::new(None) } }
+thread_local! {
+    static THREAD_INFO: RefCell<Option<ThreadInfo>> = const { RefCell::new(None) };
+    static THREAD_ID: Cell<Option<ThreadId>> = const { Cell::new(None) };
+}
 
 impl ThreadInfo {
     fn with<R, F>(f: F) -> Option<R>
@@ -30,8 +33,21 @@ impl ThreadInfo {
     }
 }
 
-pub fn current_thread_id() -> Option<ThreadId> {
-    ThreadInfo::with(|info| info.thread.id())
+#[inline]
+pub(crate) fn current_thread_id() -> ThreadId {
+    fn init(cache: &Cell<Option<ThreadId>>) -> ThreadId {
+        let id = ThreadId::new();
+        cache.set(Some(id));
+        id
+    }
+    // The `with` here's panic branch should be DCEd on platforms with
+    // `#[thread_local]`, as `THREAD_ID` is implemented to avoid needing
+    // registration of a TLS destructor on these targets.
+    //
+    // That said, this is just best-effort and not a guarantee. If we have to
+    // fallback to non-static TLS (e.g. `pthread_key_t`, `TlsAlloc`, ...),
+    // there's we have no choice but to panic.
+    THREAD_ID.with(|c: &Cell<Option<ThreadId>>| c.get().unwrap_or_else(|| init(c)))
 }
 
 pub fn current_thread() -> Option<Thread> {
@@ -43,9 +59,14 @@ pub fn stack_guard() -> Option<Guard> {
 }
 
 pub fn set(stack_guard: Option<Guard>, thread: Thread) {
+    let id = thread.id();
     THREAD_INFO.with(move |thread_info| {
         let mut thread_info = thread_info.borrow_mut();
         rtassert!(thread_info.is_none());
         *thread_info = Some(ThreadInfo { stack_guard, thread });
+    });
+    THREAD_ID.with(|c| {
+        rtassert!(c.get().is_none() || c.get() == Some(id));
+        c.set(Some(id));
     });
 }
